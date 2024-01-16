5384ecf7a39f9a58d9bb285a8d5663e9a54f6eb1
AnteN901 <antelol11@gmail.com>
2024-01-16 23:20:55 +0100

    router funktion zum einloggen hinzugefügt

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import StartView from "../views/StartView.vue";
 import LoginView from "../views/LoginView.vue"; 
 import RegisterView from "../views/RegisterView.vue";
+import { useCustomerStore } from '@/stores/CustomerStore';
 const routes = [
   {
     path: "/",
@@ -9,9 +10,17 @@ const routes = [
     component: StartView,
   },
   {
-    path: "/login",
-    name: "login",
-    component: LoginView, // Use the imported LoginView component
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+    beforeEnter: (to, from, next) => {
+      const customerStore = useCustomerStore();
+      if (customerStore.isLoggedIn) {
+        next({ name: 'start' }); // Replace 'Home' with the name of your home route
+      } else {
+        next();
+      }
+    }
   },
   {
     path: "/register",
