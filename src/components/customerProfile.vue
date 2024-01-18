16f36114c12aab22f2324222def0b7e612ab749c
Karsten <karsten.appenzeller@gmail.com>
2024-01-18 15:26:51 +0100

    skrt

--- src/components/customerProfile.vue
+++ src/components/customerProfile.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container">
     <div class="buttons-container">
-      <button v-if="showButton1" @click="showText1">Warenkorb</button>
-      <button v-if="showButton2" @click="showText2">Bestellübersicht</button>
+      <button v-if="showWarenkorbBtn" @click="WarenkorbClicked">Warenkorb</button>
+      <button v-if="showBestellHistorieBtn" @click="BestellHisorieClicked">Bestellübersicht</button>
     </div>
 
     <div v-if="!showButtons" class="text-fields-container">
       <div class="text-fields">
-        <input v-if="visibleText1" type="text" value="Text for Warenkorb" readonly class="text-field" />
-        <input v-if="visibleText2" type="text" value="Text for Bestellübersicht" readonly class="text-field" />
+        <input v-if="visibleWarenkorb" type="text" value="Text for Warenkorb" readonly class="text-field" />
+        <input v-if="visibleBestHist" type="text" value="Text for Bestellübersicht" readonly class="text-field" />
       </div>
 
       <div class="buttons-bottom">
-        <button v-if="showBackButton" @click="goBack" class="back-button">Back</button>
-        <button v-if="showBuyButton" @click="buy" class="buy-button">Kaufen</button>
+        <button v-if="showBackButton" @click="BackClicked" class="back-button">Back</button>
+        <button v-if="showBuyButton" @click="buyClicked" class="buy-button">Kaufen</button>
       </div>
     </div>
   </div>
@@ -23,40 +23,44 @@
 export default {
   data() {
     return {
-      showButton1: true,
-      showButton2: true,
+      showWarenkorbBtn: true,
+      showBestellHistorieBtn: true,
       showBuyButton: false,
       showBackButton: false,
-      visibleText1: false,
-      visibleText2: false,
+      visibleWarenkorb: false,
+      visibleBestHist: false,
     };
   },
   methods: {
-    showText1() {
+    WarenkorbClicked() {
       console.log('Warenkorb button clicked');
-      this.showButton1 = false;
-      this.visibleText1 = true;
-      this.showBuyButton = true; // Show "Kaufen" button
-      this.showBackButton = true; // Show "Back" button
+      this.showBestellHistorieBtn = false;
+      this.showWarenkorbBtn = false;
+      this.visibleWarenkorb = true;
+      this.showBuyButton = true; // Show Kaufen button
+      this.showBackButton = true; // Show Back button
     },
-    showText2() {
+    BestellHisorieClicked() {
       console.log('Bestellübersicht button clicked');
-      this.showButton2 = false;
-      this.visibleText2 = true;
-      this.showBackButton = true; // Show "Back" button
+      this.showBestellHistorieBtn = false;
+      this.showWarenkorbBtn = false;
+      this.visibleBestHist = true;
+      this.showBackButton = true; // Show Back button
+      this.showBuyButton = false; // Hide Kaufen button
+
     },
-    goBack() {
+    BackClicked() {
       console.log('Back button clicked');
-      this.showButton1 = true;
-      this.showButton2 = true;
-      this.showBuyButton = false; // Hide "Kaufen" button
-      this.showBackButton = false; // Hide "Back" button
-      this.visibleText1 = false;
-      this.visibleText2 = false;
+      this.showWarenkorbBtn = true;
+      this.showBestellHistorieBtn = true;
+      this.showBuyButton = false; // Hide Kaufen button
+      this.showBackButton = false; // Hide Back button
+      this.visibleWarenkorb = false;
+      this.visibleBestHist = false;
     },
-    buy() {
+    buyClicked() {
       console.log('Kaufen button clicked');
-      // Implement the logic for the "Kaufen" button
+      // Implement the logic for the Kaufen button
     },
   },
 };
